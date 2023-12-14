eca463b0515db2d6c234d701acdff1f1a5721a2d
Muhammad Ali <muhammadali94200@gmail.com>
2023-12-14 12:39:10 +0000

    chats section Ui done

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Chat = () => {
   return (
     <div className='chat'   >
-      chat
+      <div className="chat_Info">
+        <span>Jane</span>
+        <div className="chatIcons">
+          {/* <img src={Cam} alt=""/>
+          <img src={Add} alt=""/>
+          <img src={More} alt=""/> */}
+
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Chat/Chats.jsx
+++ src/components/Chat/Chats.jsx
@@ -9,6 +9,18 @@ const Chats = () => {
                 <p>Hello </p>
             </div>
         </div>
+        <div className="userChat">
+            <div className="userChatInfo">
+                <span>Muhammad Ali </span>
+                <p>Hello Friends and Fans </p>
+            </div>
+        </div>
+        <div className="userChat">
+            <div className="userChatInfo">
+                <span>Jane </span>
+                <p>Hello Brotha</p>
+            </div>
+        </div>
     </div>
   )
 }
