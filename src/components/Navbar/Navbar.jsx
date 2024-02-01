7309be085371c5b85d4c7ce55131c3e33a1cdb6c
Muhammad Ali <muhammadali94200@gmail.com>
2024-02-01 20:37:25 +0000

    theme changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,7 +22,8 @@ const Navbar = () => {
         <span className="logo">Campus Connect</span>
         <div className="user">
           <img src={currentUser.photoURL} alt=''/>
-            <span>{currentUser.displayName}</span>
+            <span> {currentUser.displayName}</span>
+            
             <button onClick={handleLogout}>Logout</button>
         </div>
       
