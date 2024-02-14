820c9e58f1b5c8b9936bacc41520136f689b9119
Muhammad Ali <muhammadali94200@gmail.com>
2024-02-14 16:12:19 +0000

    working on Zoom integration

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,27 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import VideocamIcon from '@mui/icons-material/Videocam';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Messages from '../Messages/Messages';
 import Input from "../Input/Input";
 import { ChatContext } from '../../context/ChatContext';
+import Zoom from '../Zoom/Zoom';
 
 const Chat = () => {
   const{data}= useContext(ChatContext);
+  const [showVideo, setShowVideo] = useState(false);
+
+  const toggleVideo = () => {
+    setShowVideo(!showVideo);
+  };
   return (
     <div className='chat'>
       <div className="chat_Info">
         <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           
-              <VideocamIcon></VideocamIcon>
-              <PersonAddIcon></PersonAddIcon>
+              <VideocamIcon onClick={toggleVideo}></VideocamIcon>
               <MoreHorizIcon></MoreHorizIcon>
          
         </div>       
       </div>
-      <Messages/>
-      <Input/>
+      {showVideo ? (
+        <>
+          <Messages />
+          <Input/>
+        </>
+      ) : (
+        <Zoom/>
+      )}
     </div>
   )
 }
---
+++ src/components/Zoom/Zoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './../../style.scss'
+const Zoom = () => {
+  return (
+    <div className='zoom'>
+      zoom
+    </div>
+  )
+}
+
+export default Zoom
