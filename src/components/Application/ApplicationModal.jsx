437161379dd28f007675aef5c050a9e951316fd3
Muhammad Ali <muhammadali94200@gmail.com>
2024-04-07 23:07:41 +0100

    fixed:styles

--- src/components/Application/ApplicationModal.jsx
+++ src/components/Application/ApplicationModal.jsx
@@ -1,32 +1,46 @@
-import { useEffect } from 'react';
-import './application.css';
-const AnnouncementModal=({closeModal,applicationPurpose,email,enrollment,name,fileUrl})=>{
-    useEffect(()=>{
-        document.body.style.overflowY="hidden";
-        return ()=>{document.body.style.overflowY="scroll";};
-    },[]);
-    return(
-      <>
+import { useEffect } from "react";
+import "./application.css";
+const AnnouncementModal = ({
+  closeModal,
+  applicationPurpose,
+  email,
+  enrollment,
+  name,
+  fileUrl,
+}) => {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
+  return (
+    <>
       <div className="modal-wrapper" onClick={closeModal}></div>
-      <div className="modal-container">
-        <div className='data-container'>
-        <h3>Application Purpose</h3>
-        <h3>{applicationPurpose}</h3>
-        <h3>Enrollment</h3>
-        <h3>{enrollment}</h3>
-        <h3>Name</h3>
-        <h3>{name}</h3>
-        <h3>Email</h3>
-        <h3>{email}</h3>
-
+      <div className="modalApplication-container">
+        <div className="data-container">
+          <h3>
+            <b>Name:</b>
+            {name}
+          </h3>
+          <h3>
+            <b>Enrollment:</b>
+            {enrollment}
+          </h3>
+          <h3>
+            <b>Application Purpose:</b>
+            {applicationPurpose}
+          </h3>
+          <h3>
+            <b>Email:</b>
+            {email}
+          </h3>
         </div>
-        <div className='image-container'>
-            <img src={fileUrl}alt="" />
+        <div className="applicationImage-container">
+          <img src={fileUrl} alt="" />
         </div>
-        
-        </div>
-        
-      </>
-    )
-  };
-  export default AnnouncementModal;
+      </div>
+    </>
+  );
+};
+export default AnnouncementModal;
